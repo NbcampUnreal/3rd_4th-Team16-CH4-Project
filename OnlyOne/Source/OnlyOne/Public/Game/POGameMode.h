9c7a30dd3010fdbdfb05e2ad153afb321d55fc12
Seonho Kim <dev.sensational@gmail.com>
2025-09-08 19:21:39 +0900

    중요 클래스 및 공통 기능 추가

---
+++ OnlyOne/Source/OnlyOne/Public/Game/POGameMode.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "POGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONLYONE_API APOGameMode : public AGameMode
+{
+	GENERATED_BODY()
+};
