2652060b9e01baa3ea82cb2615bb30df69a4d8ec
Seonho Kim <dev.sensational@gmail.com>
2025-09-11 17:37:26 +0900

    update: 서버 접속용 UI 구현

--- OnlyOne/Source/OnlyOne/Private/UI/MainMenu/POJoinServerWidget.cpp
+++ OnlyOne/Source/OnlyOne/Private/UI/MainMenu/POJoinServerWidget.cpp
@@ -2,3 +2,35 @@
 
 
 #include "UI/MainMenu/POJoinServerWidget.h"
+
+#include "Components/Button.h"
+#include "Components/EditableTextBox.h"
+#include "OnlyOne/OnlyOne.h"
+#include "UI/Common/POBaseWindow.h"
+
+void UPOJoinServerWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (WindowUI)
+	{
+		WindowUI->OnCloseWindow.AddDynamic(this, &UPOJoinServerWidget::RemoveFromParent);
+	}
+	
+	if (JoinButton)
+	{
+		JoinButton->OnClicked.AddDynamic(this, &UPOJoinServerWidget::OnJoinButtonClicked);
+	}
+}
+
+void UPOJoinServerWidget::BeginDestroy()
+{
+	Super::BeginDestroy();
+}
+
+void UPOJoinServerWidget::OnJoinButtonClicked()
+{
+	FString IP = ServerAddressTextBox->GetText().ToString();
+	FString Username = UsernameTextBox->GetText().ToString();
+	UE_LOG(POLog, Log, TEXT("IP: %s, Username: %s"), *IP, *Username);
+}
--- OnlyOne/Source/OnlyOne/Public/UI/MainMenu/POJoinServerWidget.h
+++ OnlyOne/Source/OnlyOne/Public/UI/MainMenu/POJoinServerWidget.h
@@ -7,6 +7,8 @@
 #include "UI/Common/POCustomButton.h"
 #include "POJoinServerWidget.generated.h"
 
+class UPOBaseWindow;
+class UEditableTextBox;
 /**
  * 
  */
@@ -14,4 +16,24 @@ UCLASS()
 class ONLYONE_API UPOJoinServerWidget : public UPOBaseWidget
 {
 	GENERATED_BODY()
+public:
+	virtual void NativeConstruct() override;
+	virtual void BeginDestroy() override;
+	
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UPOBaseWindow> WindowUI;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> JoinButton;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UEditableTextBox> UsernameTextBox;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UEditableTextBox> ServerAddressTextBox;
+	
+private:
+	UFUNCTION()
+	void OnJoinButtonClicked();
 };
