c40ca90cddad9d4a0b255ab5b9842fca4bed6a3b
Seonho Kim <dev.sensational@gmail.com>
2025-09-12 17:25:38 +0900

    [ADD] 서버 로비용 UI 클래스 추가

---
+++ OnlyOne/Source/OnlyOne/Public/UI/ServerLobbyMenu/POServerLobbyWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Common/POBaseWidget.h"
+#include "POServerLobbyWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONLYONE_API UPOServerLobbyWidget : public UPOBaseWidget
+{
+	GENERATED_BODY()
+};
