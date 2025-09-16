9a20ff8158564fb1e16626d07c5b5e4ff05b388f
LeeHwaRang <snow2271@gmail.com>
2025-09-16 10:42:05 +0900

    작업 저장

--- OnlyOne/Source/OnlyOne/Public/Game/POGameMode.h
+++ OnlyOne/Source/OnlyOne/Public/Game/POGameMode.h
@@ -16,7 +16,6 @@ class ONLYONE_API APOGameMode : public AGameMode
 	GENERATED_BODY()
 public:
 	virtual APlayerController* SpawnPlayerController(ENetRole InRemoteRole, const FString& Options) override;
-	virtual APlayerController* SpawnPlayerController(ENetRole InRemoteRole, FVector const& SpawnLocation, FRotator const& SpawnRotation) override;
 
 private:
 	FGenericTeamId GetTeamIdForPlayer(APlayerController* PlayerController);
