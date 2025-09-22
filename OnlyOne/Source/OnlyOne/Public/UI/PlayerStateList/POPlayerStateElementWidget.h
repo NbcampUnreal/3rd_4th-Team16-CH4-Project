6b6eb1093d73d1649d2554812a42c9270666aa3f
Seonho Kim <dev.sensational@gmail.com>
2025-09-22 19:39:36 +0900

    add: PlayerStateList용 클래스 추가

---
+++ OnlyOne/Source/OnlyOne/Public/UI/PlayerStateList/POPlayerStateElementWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Common/POBaseWidget.h"
+#include "POPlayerStateElementWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONLYONE_API UPOPlayerStateElementWidget : public UPOBaseWidget
+{
+	GENERATED_BODY()
+};
---
+++ OnlyOne/Source/OnlyOne/Public/UI/PlayerStateList/POPlayerStateListWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Common/POBaseWidget.h"
+#include "POPlayerStateListWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONLYONE_API UPOPlayerStateListWidget : public UPOBaseWidget
+{
+	GENERATED_BODY()
+};
