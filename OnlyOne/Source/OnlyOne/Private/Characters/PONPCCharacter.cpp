36553cb1cb78b74e5355173748fb3115d9c1da3f
chan <byby3534@gmail.com>
2025-09-23 18:43:10 +0900

    임시커밋

--- OnlyOne/Source/OnlyOne/Private/Characters/PONPCCharacter.cpp
+++ OnlyOne/Source/OnlyOne/Private/Characters/PONPCCharacter.cpp
@@ -8,6 +8,7 @@
 #include "Components/CapsuleComponent.h"
 #include "DataAssets/Startup/PODataAsset_StartupDataBase.h"
 #include "Controllers/PONPCController.h"
+#include "Net/UnrealNetwork.h"
 
 APONPCCharacter::APONPCCharacter()
 {
@@ -17,6 +18,16 @@ APONPCCharacter::APONPCCharacter()
 	GetCharacterMovement()->MaxWalkSpeed = 300.0f;
 
 	NpcCombatComponent = CreateDefaultSubobject<UNpcCombatComponent>(TEXT("NPC Combat Component"));
+}
+
+void APONPCCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (APOStageGameState* GS = GetWorld()->GetGameState<APOStageGameState>())
+	{
+		GS->OnPhaseChanged.AddUObject(this, &APONPCCharacter::HandleGamePhaseChanged);
+	}
 }
 
 void APONPCCharacter::PossessedBy(AController* NewController)
@@ -29,6 +40,18 @@ void APONPCCharacter::PossessedBy(AController* NewController)
 			constexpr int32 AbilityApplyLevel = 1;
 			LoadedData->GiveToAbilitySystemComponent(POAbilitySystemComponent, AbilityApplyLevel);
 		}
+	}
+}
+
+void APONPCCharacter::HandleGamePhaseChanged(EPOStagePhase NewPhase)
+{
+	if (NewPhase == EPOStagePhase::Prep)
+	{
+		GetCharacterMovement()->DisableMovement();
+	}
+	else if (NewPhase == EPOStagePhase::Active)
+	{
+		GetCharacterMovement()->SetMovementMode(MOVE_Walking);
 	}
 }
 
