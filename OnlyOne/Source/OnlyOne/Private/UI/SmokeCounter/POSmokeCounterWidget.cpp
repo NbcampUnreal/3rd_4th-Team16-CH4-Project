3c1dc94dce30b4e67b6ca85bdc283fcbef340995
Seonho Kim <dev.sensational@gmail.com>
2025-10-12 19:39:36 +0900

    add: 연막탄 카운터 구현

---
+++ OnlyOne/Source/OnlyOne/Private/UI/SmokeCounter/POSmokeCounterWidget.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/SmokeCounter/POSmokeCounterWidget.h"
+#include "Components/TextBlock.h"
+
+void UPOSmokeCounterWidget::SetSmokeCount(int32 NewCount)
+{
+	if (SmokeText)
+	{
+		SmokeText->SetText(FText::AsNumber(NewCount));
+	}
+}
+
+void UPOSmokeCounterWidget::SetSmokeText(const FText& InText)
+{
+	if (SmokeText)
+	{
+		SmokeText->SetText(InText);
+	}
+}
---
+++ OnlyOne/Source/OnlyOne/Public/UI/SmokeCounter/POSmokeCounterWidget.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Common/POBaseWidget.h"
+#include "POSmokeCounterWidget.generated.h"
+
+UCLASS()
+class ONLYONE_API UPOSmokeCounterWidget : public UPOBaseWidget
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="SmokeCounter")
+	void SetSmokeCount(int32 NewCount);
+
+	UFUNCTION(BlueprintCallable, Category="SmokeCounter")
+	void SetSmokeText(const FText& InText);
+
+protected:
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<class UTextBlock> SmokeText; 
+};
